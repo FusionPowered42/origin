16186ff9991f6392548943252dfb58a18cbfc0af
Zayd Rajab <zayd.rajab98@gmail.com>
2024-03-09 10:26:13 -0600

    Initial Commit

---
+++ Taylor AI Art/Taylor_AI_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taylor_AI_ArtApp.swift
+//  Taylor AI Art
+//
+//  Created by Zayd Rajab on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Taylor_AI_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
